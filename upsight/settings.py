1dd237470215959b14d416c93bea33a102d6608d
Fazliddin <fazliddindehkanoff@yandex.com>
2025-08-28 00:12:30 +0500

    update database conf

--- upsight/settings.py
+++ upsight/settings.py
@@ -139,23 +139,23 @@ CSRF_COOKIE_SAME_SITE = 'None'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'upsight',
-#         'USER': 'upsightuser',
-#         'PASSWORD': 'upsight2024',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'upsight',
+        'USER': 'upsightuser',
+        'PASSWORD': 'upsight2024',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
